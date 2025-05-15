85705e19ffe5f02a73cefff7dfd6c0e4770281e2
Arun Soundararajan <arunsoundararajan@Aruns-MacBook-Air.local>
2025-05-15 21:16:27 +0100

    changes

--- src/videoComponents/MainVideoPage.js
+++ src/videoComponents/MainVideoPage.js
@@ -126,9 +126,179 @@
 //   );
 // }
 
-import { useEffect, useRef, useState } from 'react';
+// import { useEffect, useRef, useState } from 'react';
+// import { useLocation } from 'react-router-dom';
+// // import { useSearchParams } from 'react-router-dom';
+// import axios from 'axios';
+// import { useDispatch, useSelector } from 'react-redux';
+// import addStream from '../redux-elements/actions/addStream';
+// import updateCallStatus from '../redux-elements/actions/updateCallStatus';
+// import createPeerConnection from '../webRTCutilities/createPeerConnection';
+// import socketConnection from '../webRTCutilities/socketConnection';
+// import ChatWindow from './ChatWindow';
+// import ActionButtons from './ActionButtons';
+// import './VideoComponents.css';
+
+// export default function MainVideoPage() {
+//   const dispatch = useDispatch();
+//   const { audio, video, haveCreatedOffer, answer } = useSelector(s => s.callStatus);
+//   const streams = useSelector(s => s.streams);
+//   // const [searchParams] = useSearchParams();
+
+//   const streamsRef = useRef(null);
+//   const pendingIce = useRef([]);
+//   const socketRef = useRef(null);
+
+//   const smallFeedEl = useRef(null);
+//   const largeFeedEl = useRef(null);
+
+//   const location = useLocation();
+
+//   let token;
+//       if (location.hash.includes('?')) {
+//         const query = location.hash.split('?')[1];             // "token=eyJ..."
+//         token = new URLSearchParams(query).get('token');       // "eyJ..."
+//       }
+
+//   // 1) INIT: validate token, connect socket, set up listeners, get media, build PC
+//   useEffect(() => {
+//     let mounted = true;
+
+//     (async () => {
+//       // const token = searchParams.get('token');
+//       // location.hash might look like "#/join-video?token=eyJ..."
+      
+//       // Validate link
+//       const { data } = await axios.post(
+//         'process.env.REACT_APP_API_URL/validate-link',
+//         { token }
+//       );
+//       if (!mounted) return;
+
+//       // Connect Socket.IO
+//       const socket = socketConnection(token);
+//       socketRef.current = socket;
+
+//       // SDP answer listener
+//       socket.on('answerToClient', ans => {
+//         console.log('[CLIENT] got answer', ans);
+//         dispatch(updateCallStatus('answer', ans));
+//       });
+
+//       // ICE candidate listener
+//       socket.on('iceToClient', ({ iceC }) => {
+//         console.log('[CLIENT] got ICE', iceC);
+//         const pc = streamsRef.current?.remote1?.peerConnection;
+//         if (pc?.remoteDescription) {
+//           pc.addIceCandidate(iceC).catch(console.error);
+//         } else {
+//           pendingIce.current.push(iceC);
+//         }
+//       });
+
+//       // VIDEO toggle listener
+//       socket.on('toggleVideo', ({ off }) => {
+//         console.log('[CLIENT] toggleVideo remote:', off);
+//         largeFeedEl.current.style.display = off ? 'none' : 'block';
+//       });
+
+//       // Get local media (start enabled)
+//       const localStream = await navigator.mediaDevices.getUserMedia({
+//         video: true,
+//         audio: true,
+//       });
+//       if (!mounted) return;
+//       smallFeedEl.current.srcObject = localStream;
+
+//       dispatch(updateCallStatus('haveMedia', true));
+//       dispatch(addStream('localStream', localStream));
+//       dispatch(updateCallStatus('audio', 'enabled'));
+//       dispatch(updateCallStatus('video', 'enabled'));
+
+//       // Build PeerConnection
+//       const { peerConnection, remoteStream } = await createPeerConnection(iceC => {
+//         socket.emit('iceToServer', { who: 'client', iceC });
+//       });
+//       localStream.getTracks().forEach(track =>
+//         peerConnection.addTrack(track, localStream)
+//       );
+//       dispatch(addStream('remote1', remoteStream, peerConnection));
+//       largeFeedEl.current.srcObject = remoteStream;
+//       largeFeedEl.current.style.display = 'block';
+//       console.log('[DEBUG] remote video attached and shown');
+//     })();
+
+//     return () => {
+//       mounted = false;
+//       socketRef.current?.disconnect();
+//     };
+//   }, [dispatch, searchParams]);
+
+//   // keep streamsRef for ICE & offer
+//   useEffect(() => {
+//     if (streams.remote1) streamsRef.current = streams;
+//   }, [streams]);
+
+//   // 2) SEND OFFER once media & PC are ready
+//   useEffect(() => {
+//     if (
+//       audio === 'enabled' &&
+//       video === 'enabled' &&
+//       !haveCreatedOffer &&
+//       streamsRef.current?.remote1?.peerConnection
+//     ) {
+//       ;(async () => {
+//         const pc = streamsRef.current.remote1.peerConnection;
+//         console.log('[CLIENT] creating SDP offer');
+//         const offer = await pc.createOffer();
+//         await pc.setLocalDescription(offer);
+//         console.log('[CLIENT] sending SDP offer');
+//         socketRef.current.emit('newOffer', { offer });
+//         dispatch(updateCallStatus('haveCreatedOffer', true));
+//       })().catch(console.error);
+//     }
+//   }, [audio, video, haveCreatedOffer, dispatch]);
+
+//   // 3) APPLY ANSWER + FLUSH ICE
+//   useEffect(() => {
+//     if (answer && haveCreatedOffer) {
+//       console.log('[CLIENT] applying SDP answer');
+//       const pc = streamsRef.current.remote1.peerConnection;
+//       pc.setRemoteDescription(answer).then(() => {
+//         pendingIce.current.forEach(c => pc.addIceCandidate(c).catch(console.error));
+//         pendingIce.current = [];
+//         console.log('[CLIENT] ICE buffer flushed');
+//       });
+//     }
+//   }, [answer, haveCreatedOffer]);
+
+//   return (
+//     <div className="video-chat-wrapper">
+//       <video
+//         ref={largeFeedEl}
+//         className="remote-video"
+//         autoPlay
+//         playsInline
+//       />
+//       <video
+//         ref={smallFeedEl}
+//         className="local-video"
+//         autoPlay
+//         playsInline
+//         muted
+//       />
+//       <ChatWindow />
+//       <ActionButtons
+//         socket={socketRef.current}
+//         smallFeedEl={smallFeedEl}
+//         largeFeedEl={largeFeedEl}
+//       />
+//     </div>
+//   );
+// }
+
+import { useEffect, useRef } from 'react';
 import { useLocation } from 'react-router-dom';
-// import { useSearchParams } from 'react-router-dom';
 import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
 import addStream from '../redux-elements/actions/addStream';
@@ -143,7 +313,6 @@ export default function MainVideoPage() {
   const dispatch = useDispatch();
   const { audio, video, haveCreatedOffer, answer } = useSelector(s => s.callStatus);
   const streams = useSelector(s => s.streams);
-  const [searchParams] = useSearchParams();
 
   const streamsRef = useRef(null);
   const pendingIce = useRef([]);
@@ -152,22 +321,26 @@ export default function MainVideoPage() {
   const smallFeedEl = useRef(null);
   const largeFeedEl = useRef(null);
 
-  // 1) INIT: validate token, connect socket, set up listeners, get media, build PC
+  const location = useLocation();
+  let token;
+  if (location.hash.includes('?')) {
+    const query = location.hash.split('?')[1];
+    token = new URLSearchParams(query).get('token');
+  }
+
+  // 1) INIT
   useEffect(() => {
     let mounted = true;
 
     (async () => {
-      // const token = searchParams.get('token');
-      const location = useLocation();
-      // location.hash might look like "#/join-video?token=eyJ..."
-      let token;
-      if (location.hash.includes('?')) {
-        const query = location.hash.split('?')[1];             // "token=eyJ..."
-        token = new URLSearchParams(query).get('token');       // "eyJ..."
+      if (!token) {
+        console.error('No token found in URL');
+        return;
       }
+
       // Validate link
       const { data } = await axios.post(
-        'process.env.REACT_APP_API_URL/validate-link',
+        `${process.env.REACT_APP_API_URL}/validate-link`,
         { token }
       );
       if (!mounted) return;
@@ -176,15 +349,10 @@ export default function MainVideoPage() {
       const socket = socketConnection(token);
       socketRef.current = socket;
 
-      // SDP answer listener
       socket.on('answerToClient', ans => {
-        console.log('[CLIENT] got answer', ans);
         dispatch(updateCallStatus('answer', ans));
       });
-
-      // ICE candidate listener
       socket.on('iceToClient', ({ iceC }) => {
-        console.log('[CLIENT] got ICE', iceC);
         const pc = streamsRef.current?.remote1?.peerConnection;
         if (pc?.remoteDescription) {
           pc.addIceCandidate(iceC).catch(console.error);
@@ -192,14 +360,11 @@ export default function MainVideoPage() {
           pendingIce.current.push(iceC);
         }
       });
-
-      // VIDEO toggle listener
       socket.on('toggleVideo', ({ off }) => {
-        console.log('[CLIENT] toggleVideo remote:', off);
         largeFeedEl.current.style.display = off ? 'none' : 'block';
       });
 
-      // Get local media (start enabled)
+      // Get local media
       const localStream = await navigator.mediaDevices.getUserMedia({
         video: true,
         audio: true,
@@ -222,21 +387,20 @@ export default function MainVideoPage() {
       dispatch(addStream('remote1', remoteStream, peerConnection));
       largeFeedEl.current.srcObject = remoteStream;
       largeFeedEl.current.style.display = 'block';
-      console.log('[DEBUG] remote video attached and shown');
     })();
 
     return () => {
       mounted = false;
       socketRef.current?.disconnect();
     };
-  }, [dispatch, searchParams]);
+  }, [dispatch, token]);
 
-  // keep streamsRef for ICE & offer
+  // Keep streamsRef
   useEffect(() => {
     if (streams.remote1) streamsRef.current = streams;
   }, [streams]);
 
-  // 2) SEND OFFER once media & PC are ready
+  // 2) SEND OFFER
   useEffect(() => {
     if (
       audio === 'enabled' &&
@@ -244,12 +408,10 @@ export default function MainVideoPage() {
       !haveCreatedOffer &&
       streamsRef.current?.remote1?.peerConnection
     ) {
-      ;(async () => {
+      (async () => {
         const pc = streamsRef.current.remote1.peerConnection;
-        console.log('[CLIENT] creating SDP offer');
         const offer = await pc.createOffer();
         await pc.setLocalDescription(offer);
-        console.log('[CLIENT] sending SDP offer');
         socketRef.current.emit('newOffer', { offer });
         dispatch(updateCallStatus('haveCreatedOffer', true));
       })().catch(console.error);
@@ -259,31 +421,18 @@ export default function MainVideoPage() {
   // 3) APPLY ANSWER + FLUSH ICE
   useEffect(() => {
     if (answer && haveCreatedOffer) {
-      console.log('[CLIENT] applying SDP answer');
       const pc = streamsRef.current.remote1.peerConnection;
       pc.setRemoteDescription(answer).then(() => {
         pendingIce.current.forEach(c => pc.addIceCandidate(c).catch(console.error));
         pendingIce.current = [];
-        console.log('[CLIENT] ICE buffer flushed');
       });
     }
   }, [answer, haveCreatedOffer]);
 
   return (
     <div className="video-chat-wrapper">
-      <video
-        ref={largeFeedEl}
-        className="remote-video"
-        autoPlay
-        playsInline
-      />
-      <video
-        ref={smallFeedEl}
-        className="local-video"
-        autoPlay
-        playsInline
-        muted
-      />
+      <video ref={largeFeedEl} className="remote-video" autoPlay playsInline />
+      <video ref={smallFeedEl} className="local-video" autoPlay playsInline muted />
       <ChatWindow />
       <ActionButtons
         socket={socketRef.current}
@@ -293,3 +442,4 @@ export default function MainVideoPage() {
     </div>
   );
 }
+
--- src/videoComponents/ProMainVideoPage.js
+++ src/videoComponents/ProMainVideoPage.js
@@ -107,8 +107,167 @@
 //   );
 // }
 
+// ############
+
+
+// import { useEffect, useRef } from 'react';
+// // import { useSearchParams } from 'react-router-dom';
+// import { useLocation } from 'react-router-dom';
+// import axios from 'axios';
+// import { useDispatch, useSelector } from 'react-redux';
+// import addStream from '../redux-elements/actions/addStream';
+// import updateCallStatus from '../redux-elements/actions/updateCallStatus';
+// import createPeerConnection from '../webRTCutilities/createPeerConnection';
+// import socketConnection from '../webRTCutilities/socketConnection';
+// import ChatWindow from './ChatWindow';
+// import ActionButtons from './ActionButtons';
+// import './VideoComponents.css';
+
+// export default function ProMainVideoPage() {
+//   const dispatch = useDispatch();
+//   const { offer, haveCreatedAnswer } = useSelector(s => s.callStatus);
+//   const streams = useSelector(s => s.streams);
+//   // const [searchParams] = useSearchParams();
+
+//   const location = useLocation();
+// // location.hash might look like "#/join-video?token=eyJ..."
+// let token;
+// if (location.hash.includes('?')) {
+//   const query = location.hash.split('?')[1];             // "token=eyJ..."
+//   token = new URLSearchParams(query).get('token');       // "eyJ..."
+// }
+
+//   const streamsRef = useRef(null);
+//   const pendingIce = useRef([]);
+//   const socketRef = useRef(null);
+
+//   const smallFeedEl = useRef(null);
+//   const largeFeedEl = useRef(null);
+
+//   // 1) INIT
+//   useEffect(() => {
+//     let mounted = true;
+
+//     (async () => {
+//       // const token = searchParams.get('token');
+//       await axios.post(  `${process.env.REACT_APP_API_URL}/validate-link`,
+//          { token });
+
+//       // Connect Socket.IO
+//       const socket = socketConnection(token);
+//       socketRef.current = socket;
+
+//       // Listen for client offer
+//       socket.on('newOfferWaiting', ({ offer }) => {
+//         console.log('[PRO] got SDP offer', offer);
+//         dispatch(updateCallStatus('offer', offer));
+//       });
+
+//       // Listen for ICE
+//       socket.on('iceToClient', ({ iceC }) => {
+//         console.log('[PRO] got ICE', iceC);
+//         const pc = streamsRef.current?.remote1?.peerConnection;
+//         if (pc?.remoteDescription) {
+//           pc.addIceCandidate(iceC).catch(console.error);
+//         } else {
+//           pendingIce.current.push(iceC);
+//         }
+//       });
+
+//       // VIDEO toggle listener
+//       socket.on('toggleVideo', ({ off }) => {
+//         console.log('[PRO] toggleVideo remote:', off);
+//         largeFeedEl.current.style.display = off ? 'none' : 'block';
+//       });
+
+//       // Get local media
+//       const localStream = await navigator.mediaDevices.getUserMedia({
+//         video: true,
+//         audio: true,
+//       });
+//       if (!mounted) return;
+//       smallFeedEl.current.srcObject = localStream;
+
+//       dispatch(updateCallStatus('haveMedia', true));
+//       dispatch(addStream('localStream', localStream));
+//       dispatch(updateCallStatus('audio', 'enabled'));
+//       dispatch(updateCallStatus('video', 'enabled'));
+
+//       // Build PeerConnection
+//       const { peerConnection, remoteStream } = await createPeerConnection(iceC => {
+//         socket.emit('iceToServer', { who: 'professional', iceC });
+//       });
+//       localStream.getTracks().forEach(track =>
+//         peerConnection.addTrack(track, localStream)
+//       );
+//       dispatch(addStream('remote1', remoteStream, peerConnection));
+//       largeFeedEl.current.srcObject = remoteStream;
+//       largeFeedEl.current.style.display = 'block';
+//       console.log('[DEBUG] remote video attached and shown');
+//     })();
+
+//     return () => {
+//       mounted = false;
+//       socketRef.current?.disconnect();
+//     };
+//   }, [dispatch, searchParams]);
+
+//   // keep streamsRef
+//   useEffect(() => {
+//     if (streams.remote1) streamsRef.current = streams;
+//   }, [streams]);
+
+//   // 2) ANSWER & FLUSH ICE
+//   useEffect(() => {
+//     if (!offer || haveCreatedAnswer || !streamsRef.current?.remote1?.peerConnection)
+//       return;
+//     ;(async () => {
+//       const pc = streamsRef.current.remote1.peerConnection;
+//       console.log('[PRO] setting remote description');
+//       await pc.setRemoteDescription(offer);
+
+//       pendingIce.current.forEach(c => pc.addIceCandidate(c).catch(console.error));
+//       pendingIce.current = [];
+
+//       console.log('[PRO] creating SDP answer');
+//       const answer = await pc.createAnswer();
+//       await pc.setLocalDescription(answer);
+
+//       console.log('[PRO] sending SDP answer');
+//       socketRef.current.emit('newAnswer', { answer });
+//       dispatch(updateCallStatus('haveCreatedAnswer', true));
+//       dispatch(updateCallStatus('answer', answer));
+//     })().catch(console.error);
+//   }, [offer, haveCreatedAnswer, dispatch]);
+
+//   return (
+//     <div className="video-chat-wrapper">
+//       <video
+//         ref={largeFeedEl}
+//         className="remote-video"
+//         autoPlay
+//         playsInline
+//       />
+//       <video
+//         ref={smallFeedEl}
+//         className="local-video"
+//         autoPlay
+//         playsInline
+//         muted
+//       />
+//       <ChatWindow />
+//       <ActionButtons
+//         socket={socketRef.current}
+//         smallFeedEl={smallFeedEl}
+//         largeFeedEl={largeFeedEl}
+//       />
+//     </div>
+//   );
+// }
+
+
+
 import { useEffect, useRef } from 'react';
-// import { useSearchParams } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
@@ -124,15 +283,13 @@ export default function ProMainVideoPage() {
   const dispatch = useDispatch();
   const { offer, haveCreatedAnswer } = useSelector(s => s.callStatus);
   const streams = useSelector(s => s.streams);
-  const [searchParams] = useSearchParams();
 
   const location = useLocation();
-// location.hash might look like "#/join-video?token=eyJ..."
-let token;
-if (location.hash.includes('?')) {
-  const query = location.hash.split('?')[1];             // "token=eyJ..."
-  token = new URLSearchParams(query).get('token');       // "eyJ..."
-}
+  let token;
+  if (location.hash.includes('?')) {
+    const query = location.hash.split('?')[1];
+    token = new URLSearchParams(query).get('token');
+  }
 
   const streamsRef = useRef(null);
   const pendingIce = useRef([]);
@@ -146,23 +303,25 @@ if (location.hash.includes('?')) {
     let mounted = true;
 
     (async () => {
-      // const token = searchParams.get('token');
-      await axios.post(  `${process.env.REACT_APP_API_URL}/validate-link`,
-         { token });
+      if (!token) {
+        console.error('No token in URL');
+        return;
+      }
+
+      // Validate link
+      await axios.post(
+        `${process.env.REACT_APP_API_URL}/validate-link`,
+        { token }
+      );
 
       // Connect Socket.IO
       const socket = socketConnection(token);
       socketRef.current = socket;
 
-      // Listen for client offer
       socket.on('newOfferWaiting', ({ offer }) => {
-        console.log('[PRO] got SDP offer', offer);
         dispatch(updateCallStatus('offer', offer));
       });
-
-      // Listen for ICE
       socket.on('iceToClient', ({ iceC }) => {
-        console.log('[PRO] got ICE', iceC);
         const pc = streamsRef.current?.remote1?.peerConnection;
         if (pc?.remoteDescription) {
           pc.addIceCandidate(iceC).catch(console.error);
@@ -170,10 +329,7 @@ if (location.hash.includes('?')) {
           pendingIce.current.push(iceC);
         }
       });
-
-      // VIDEO toggle listener
       socket.on('toggleVideo', ({ off }) => {
-        console.log('[PRO] toggleVideo remote:', off);
         largeFeedEl.current.style.display = off ? 'none' : 'block';
       });
 
@@ -200,16 +356,15 @@ if (location.hash.includes('?')) {
       dispatch(addStream('remote1', remoteStream, peerConnection));
       largeFeedEl.current.srcObject = remoteStream;
       largeFeedEl.current.style.display = 'block';
-      console.log('[DEBUG] remote video attached and shown');
     })();
 
     return () => {
       mounted = false;
       socketRef.current?.disconnect();
     };
-  }, [dispatch, searchParams]);
+  }, [dispatch, token]);
 
-  // keep streamsRef
+  // Keep streamsRef
   useEffect(() => {
     if (streams.remote1) streamsRef.current = streams;
   }, [streams]);
@@ -218,40 +373,26 @@ if (location.hash.includes('?')) {
   useEffect(() => {
     if (!offer || haveCreatedAnswer || !streamsRef.current?.remote1?.peerConnection)
       return;
-    ;(async () => {
+    (async () => {
       const pc = streamsRef.current.remote1.peerConnection;
-      console.log('[PRO] setting remote description');
       await pc.setRemoteDescription(offer);
 
       pendingIce.current.forEach(c => pc.addIceCandidate(c).catch(console.error));
       pendingIce.current = [];
 
-      console.log('[PRO] creating SDP answer');
       const answer = await pc.createAnswer();
       await pc.setLocalDescription(answer);
 
-      console.log('[PRO] sending SDP answer');
       socketRef.current.emit('newAnswer', { answer });
       dispatch(updateCallStatus('haveCreatedAnswer', true));
       dispatch(updateCallStatus('answer', answer));
     })().catch(console.error);
-  }, [offer, haveCreatedAnswer, dispatch]);
+  }, [dispatch, offer, haveCreatedAnswer]);
 
   return (
     <div className="video-chat-wrapper">
-      <video
-        ref={largeFeedEl}
-        className="remote-video"
-        autoPlay
-        playsInline
-      />
-      <video
-        ref={smallFeedEl}
-        className="local-video"
-        autoPlay
-        playsInline
-        muted
-      />
+      <video ref={largeFeedEl} className="remote-video" autoPlay playsInline />
+      <video ref={smallFeedEl} className="local-video" autoPlay playsInline muted />
       <ChatWindow />
       <ActionButtons
         socket={socketRef.current}
@@ -261,3 +402,4 @@ if (location.hash.includes('?')) {
     </div>
   );
 }
+
